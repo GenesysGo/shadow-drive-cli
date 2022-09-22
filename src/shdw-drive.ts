cb60333a032546b4a776934fc52ffdcf79200fe8
Tracy <tracy@tracycodes.com>
2022-09-22 12:22:24 -0700

    Bump version

--- src/shdw-drive.ts
+++ src/shdw-drive.ts
@@ -26,7 +26,7 @@ import { ShadowDriveResponse, ShdwDrive } from "@shadow-drive/sdk";
 import { from, map, mergeMap, tap, toArray } from "rxjs";
 import mime from "mime-types";
 
-program.version("0.3.3");
+program.version("0.3.4");
 program.description(
     "CLI for interacting with Shade Drive. This tool uses Solana's Mainnet-Beta network with an internal RPC configuration. It does not use your local Solana configurations."
 );
