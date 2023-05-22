f7208f2eee1e03d68ff77a29e1911550b56f0d59
Khalil Brown <khalilnafis@gmail.com>
2023-05-22 16:15:19 -0400

    fix- display last fee

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -185,7 +185,9 @@ export async function getFormattedStorageAccounts(
             pubkey: account?.identifier
                 ? new anchor.web3.PublicKey(account.storage_account)
                 : null,
-            lastFeeEpoch: account?.identifier ? account.last_fee_epoch : null,
+            lastFeeEpoch: account?.last_fee_epoch
+                ? account.last_fee_epoch
+                : null,
             toBeDeleted: account?.identifier ? account.to_be_deleted : null,
             immutable: account?.identifier ? account.immutable : null,
             version: account?.identifier ? account.version : null,
